9a71c3f1612e13b0933fa4d9f79eabe672b39e65
Tik77 <tigran.matevosyan.1998@gmail.com>
2019-06-11 19:53:46 +0400

    new merging parts hmmmmm

--- app/index.php
+++ app/index.php
@@ -1,10 +1,9 @@
 <?php $page = "";
 $title = "Home";
-require("../templates/header.php");
+require("templates/header.php");
 if ($page == "") {
-    require '../templates/index.php';
-
+    require 'templates/index.php';
 }
-require '../templates/aside.php';
-require '../templates/footer.php';
+require 'templates/aside.php';
+require 'templates/footer.php';
 require("function.php");
