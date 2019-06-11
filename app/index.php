8cd519fcd47366d67414010eadd87e6783e6d11d
Tik77 <tigran.matevosyan.1998@gmail.com>
2019-06-11 19:44:34 +0400

    new merging parts ._.

--- app/index.php
+++ app/index.php
@@ -3,6 +3,8 @@ $title = "Home";
 require("../templates/header.php");
 if ($page == "") {
     require '../templates/index.php';
-    require '../templates/aside.php';
-    require '../templates/footer.php';
+
 }
+require '../templates/aside.php';
+require '../templates/footer.php';
+require("function.php");
